86671f2c4172155ad9e4d6cd6b87f837ee1e1230
nickscaglione <nicholas.a.scaglione@gmail.com>
2017-01-17 16:36:54 -0500

    change port to localhost:8000

--- routes/index.js
+++ routes/index.js
@@ -4,39 +4,35 @@ const pg = require('pg');
 const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dogester';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.post('/dogs', (req, res, next) => {
-  const results = [];
-  // Grab data from http request
-  const data = {name: req.body.name, image_url: req.body.image_url, score: req.body.score};
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Insert Data
-    client.query('INSERT INTO dogs(name, image_url, score) values($1, $2, $3)',
-    [data.name, data.image_url, data.score]);
-    // SQL Query > Select Data
-    const query = 2query('SELECT * FROM dogs ORDER BY id ASC');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
-});
+// router.post('/dogs', (req, res, next) => {
+//   const results = [];
+//   // Grab data from http request
+//   const data = {name: req.body.name, image_url: req.body.image_url, score: req.body.score};
+//   // Get a Postgres client from the connection pool
+//   pg.connect(connectionString, (err, client, done) => {
+//     // Handle connection errors
+//     if(err) {
+//       done();
+//       console.log(err);
+//       return res.status(500).json({success: false, data: err});
+//     }
+//     // SQL Query > Insert Data
+//     client.query('INSERT INTO dogs(name, image_url, score) values($1, $2, $3)',
+//     [data.name, data.image_url, data.score]);
+//     // SQL Query > Select Data
+//     const query = 2query('SELECT * FROM dogs ORDER BY id ASC');
+//     // Stream results back one row at a time
+//     query.on('row', (row) => {
+//       results.push(row);
+//     });
+//     // After all data is returned, close connection and return results
+//     query.on('end', () => {
+//       done();
+//       return res.json(results);
+//     });
+//   });
+// });
 
 router.get('/dogs', (req, res, next) => {
   const results = [];
